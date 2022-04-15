33681908ce833a6219ecdb0155cc59fd6455b61b
landmanbester <lbester@ska.ac.za>
2022-04-15 15:04:45 +0200

    Add scol option to flagchi2

--- surfvis/flagchi2.py
+++ surfvis/flagchi2.py
@@ -18,14 +18,24 @@ from daskms import xds_from_ms, xds_from_table, xds_to_table
 
 def create_parser():
     parser = OptionParser(usage='%prog [options] msname')
-    parser.add_option('--rcol', default='RESIDUAL', help='Residual column (default = RESIDUAL)')
-    parser.add_option('--wcol', default='WEIGHT_SPECTRUM', help='Weight column (default = WEIGHT_SPECTRUM)')
-    parser.add_option('--fcol', default='FLAG', help='Flag column (default = FLAG)')
-    parser.add_option('--sigma', default=10, type=float, help='chisq threshold (default = 25)')
-    parser.add_option('--nthreads', default=4, type=int, help='Number of dask threads to use')
-    parser.add_option('--nrows', default=10000, type=int, help='Number of rows in each chunk (default=10000)')
-    parser.add_option('--nfreqs', default=128, type=int, help='Number of frequencies in a chunk (default=128)')
-    parser.add_option("--use-corrs", type=str, help='Comma seprated list of correlations to use (do not use spaces)')
+    parser.add_option('--rcol', default='RESIDUAL',
+                      help='Residual column (default = RESIDUAL)')
+    parser.add_option('--scol',
+                      help='sigma column to use. Overides wcol if provided.')
+    parser.add_option('--wcol', default='WEIGHT_SPECTRUM',
+                      help='Weight column (default = WEIGHT_SPECTRUM)')
+    parser.add_option('--fcol', default='FLAG',
+                      help='Flag column (default = FLAG)')
+    parser.add_option('--sigma', default=10, type=float,
+                      help='chisq threshold (default = 25)')
+    parser.add_option('--nthreads', default=4, type=int,
+                      help='Number of dask threads to use')
+    parser.add_option('--nrows', default=10000, type=int,
+                      help='Number of rows in each chunk (default=10000)')
+    parser.add_option('--nfreqs', default=128, type=int,
+                      help='Number of frequencies in a chunk (default=128)')
+    parser.add_option("--use-corrs", type=str,
+                      help='Comma seprated list of correlations to use (do not use spaces)')
     return parser
 
 def main():
@@ -40,11 +50,18 @@ def main():
 
     schema = {}
     schema[options.rcol] = {'dims': ('chan', 'corr')}
-    schema[options.wcol] = {'dims': ('chan', 'corr')}
     schema[options.fcol] = {'dims': ('chan', 'corr')}
+    columns = [options.rcol, options.fcol]
+    if options.scol is not None:
+        schema[options.scol] = {'dims': ('chan', 'corr')}
+        columns.append(options.scol)
+    else:
+        schema[options.wcol] = {'dims': ('chan', 'corr')}
+        columns.append(options.wcol)
+
 
     xds = xds_from_ms(msname,
-                      columns=[options.rcol, options.wcol, options.fcol],
+                      columns=columns,
                       chunks={'row': options.nrows, 'chan': options.nfreqs},
                       group_cols=['FIELD_ID', 'DATA_DESC_ID', 'SCAN_NUMBER'],
                       table_schema=schema)
@@ -62,7 +79,11 @@ def main():
     out_data = []
     for i, ds in enumerate(xds):
         resid = ds.get(options.rcol).data
-        weight = ds.get(options.wcol).data
+        if options.scol is not None:
+            sigma = ds.get(options.wcol).data
+            weight = 1/sigma**2
+        else:
+            weight = ds.get(options.wcol).data
         flag = ds.get(options.fcol).data
 
         uflag = flagchisq(resid, weight, flag, tuple(use_corrs), sigma=options.sigma)
